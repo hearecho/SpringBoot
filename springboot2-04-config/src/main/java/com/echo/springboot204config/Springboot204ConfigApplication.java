d4c772a0b9219e99a1f227457fa7ed3dea846fb9
echo <1540302560@qq.com>
2018-08-12 21:43:51 +0800

    springboot2-04-config

---
+++ springboot2-04-config/src/main/java/com/echo/springboot204config/Springboot204ConfigApplication.java
@@ -0,0 +1,12 @@
+package com.echo.springboot204config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot204ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot204ConfigApplication.class, args);
+	}
+}
