36314065b476a29014dacbcb6aee0a024dd2a6dd
echo <1540302560@qq.com>
2018-08-23 19:11:03 +0800

    springboot日志

---
+++ springboot2-05-log/src/main/java/com/echo/springboot205log/Springboot205LogApplication.java
@@ -0,0 +1,12 @@
+package com.echo.springboot205log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot205LogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot205LogApplication.class, args);
+	}
+}
---
+++ springboot2-05-log/src/main/resources/demo.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("hello func hello")
+}
+func main()  {
+	/*
+		快注释
+	 */
+	 //行注释
+	 hello()
+	 fmt.Println("helloworld")
+}
